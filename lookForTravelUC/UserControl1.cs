7a6b68070e4d751a7d84d420c096a49df7c56fc2
aymane <aymaneaymane1337@gmail.com>
2022-01-07 16:19:29 +0100

    100% again

--- lookForTravelUC/UserControl1.cs
+++ lookForTravelUC/UserControl1.cs
@@ -12,30 +12,20 @@ namespace lookForTravelUC
 {
     public partial class mainUC: UserControl
     {
+        private DataTable _origineCities = new DataTable();
+        private DataTable _destinationCities = new DataTable();
+
+        public DataTable origineCities { set { _origineCities = value; } }
+        public DataTable destinationCities { set { _destinationCities = value; } }
         public mainUC()
         {
             InitializeComponent();
         }
         private void mainUC_Load(object sender, EventArgs e) {
-            String[] listOfCities = new string[] {
-                "city", "Casablanca", "El Kelaa des Srarhna", "Fès", "Tangier", "Marrakech", "Sale",
-                "Rabat", "Meknès", "Kenitra", "Agadir", "Oujda-Angad", "Tétouan", "Taourirt", "Temara",
-                "Safi", "Laâyoune", "Mohammedia", "Kouribga", "Béni Mellal", "El Jadid", "Ait Melloul",
-                "Nador", "Taza", "Settat", "Barrechid", "Al Khmissat", "Inezgane", "Ksar El Kebir", "Larache",
-                "Guelmim", "Khénifra", "Berkane", "Bouskoura", "Al Fqih Ben Çalah", "Oued Zem", "Sidi Slimane",
-                "Errachidia", "Guercif", "Oulad Teïma", "Ad Dakhla", "Ben Guerir", "Wislane", "Tiflet", "Lqoliaa",
-                "Taroudannt", "Sefrou", "Essaouira", "Fnidq", "Ait Ali", "Sidi Qacem", "Tiznit", "Moulay Abdallah",
-                "Tan-Tan", "Warzat", "Youssoufia", "Sa’ada", "Martil", "Aïn Harrouda", "Skhirate", "Ouezzane",
-                "Sidi Yahya Zaer", "Benslimane", "Al Hoceïma", "Beni Enzar", "M’diq", "Sidi Bennour", "Midalt",
-                "Azrou", "Ain El Aouda", "Beni Yakhlef", "Semara", "Ad Darwa", "Al Aaroui", "Qasbat Tadla", "Boujad",
-                "Jerada", "Chefchaouene", "Mrirt", "Sidi Mohamed Lahmar", "Tineghir", "El Aïoun", "Azemmour", "Temsia",
-                "Zoumi", "Laouamra", "Zagora", "Ait Ourir", "Sidi Bibi", "Aziylal", "Sidi Yahia El Gharb", "Biougra",
-                "Taounate", "Bouznika", "Aourir", "Zaïo", "Aguelmous", "El Hajeb", "Mnasra", "Mediouna", "Zeghanghane",
-                "Imzouren", "Loudaya", "Oulad Zemam", "Bou Ahmed", "Tit Mellil", "Arbaoua", "Douar Oulad Hssine",
-                "Bahharet Oulad Ayyad", "Mechraa Bel Ksiri", "Mograne"
-            };
-            destCityCB.Items.AddRange(listOfCities);
-            origineCityCB.Items.AddRange(listOfCities);
+            origineCityCB.DataSource = _origineCities;
+            origineCityCB.Text = string.Empty;
+            destCityCB.DataSource = _destinationCities;
+            destCityCB.Text = string.Empty;
             datePicker.MinDate = DateTime.Now;
         }
         private void seachBtn_Click(object sender, EventArgs e) {
