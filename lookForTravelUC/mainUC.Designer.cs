07effcddda0d2223e921ff573aff4d7d1c00564d
aymane <aymaneaymane1337@gmail.com>
2022-01-07 22:14:37 +0100

    80%

--- lookForTravelUC/mainUC.Designer.cs
+++ lookForTravelUC/mainUC.Designer.cs
@@ -30,12 +30,17 @@
         {
             this.travelCB = new Guna.UI2.WinForms.Guna2ComboBox();
             this.datePicker = new Guna.UI2.WinForms.Guna2DateTimePicker();
-            this.seachBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.searchBtn = new Guna.UI2.WinForms.Guna2Button();
             this.numOfPassgersTB = new Guna.UI2.WinForms.Guna2NumericUpDown();
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
             this.companyNameLabel = new Guna.UI.WinForms.GunaLabel();
+            this.searchResultPanel = new Guna.UI.WinForms.GunaGradient2Panel();
+            this.getTicketLbl = new Guna.UI.WinForms.GunaLinkLabel();
+            this.messageLabel = new Guna.UI.WinForms.GunaLabel();
+            this.datePickerValueLbl = new Guna.UI.WinForms.GunaLabel();
             ((System.ComponentModel.ISupportInitialize)(this.numOfPassgersTB)).BeginInit();
             this.gunaPanel1.SuspendLayout();
+            this.searchResultPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // travelCB
@@ -51,7 +56,7 @@
             this.travelCB.HoverState.Parent = this.travelCB;
             this.travelCB.ItemHeight = 30;
             this.travelCB.ItemsAppearance.Parent = this.travelCB;
-            this.travelCB.Location = new System.Drawing.Point(22, 69);
+            this.travelCB.Location = new System.Drawing.Point(92, 69);
             this.travelCB.Name = "travelCB";
             this.travelCB.ShadowDecoration.Parent = this.travelCB;
             this.travelCB.Size = new System.Drawing.Size(287, 36);
@@ -64,7 +69,7 @@
             this.datePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.datePicker.HoverState.Parent = this.datePicker;
-            this.datePicker.Location = new System.Drawing.Point(187, 117);
+            this.datePicker.Location = new System.Drawing.Point(257, 117);
             this.datePicker.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
             this.datePicker.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
             this.datePicker.Name = "datePicker";
@@ -73,21 +78,21 @@
             this.datePicker.TabIndex = 1;
             this.datePicker.Value = new System.DateTime(2022, 1, 6, 14, 37, 30, 821);
             // 
-            // seachBtn
+            // searchBtn
             // 
-            this.seachBtn.CheckedState.Parent = this.seachBtn;
-            this.seachBtn.CustomImages.Parent = this.seachBtn;
-            this.seachBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(116)))), ((int)(((byte)(212)))));
-            this.seachBtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
-            this.seachBtn.ForeColor = System.Drawing.Color.White;
-            this.seachBtn.HoverState.Parent = this.seachBtn;
-            this.seachBtn.Location = new System.Drawing.Point(22, 165);
-            this.seachBtn.Name = "seachBtn";
-            this.seachBtn.ShadowDecoration.Parent = this.seachBtn;
-            this.seachBtn.Size = new System.Drawing.Size(287, 45);
-            this.seachBtn.TabIndex = 4;
-            this.seachBtn.Text = "Search";
-            this.seachBtn.Click += new System.EventHandler(this.seachBtn_Click);
+            this.searchBtn.CheckedState.Parent = this.searchBtn;
+            this.searchBtn.CustomImages.Parent = this.searchBtn;
+            this.searchBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(116)))), ((int)(((byte)(212)))));
+            this.searchBtn.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.searchBtn.ForeColor = System.Drawing.Color.White;
+            this.searchBtn.HoverState.Parent = this.searchBtn;
+            this.searchBtn.Location = new System.Drawing.Point(92, 165);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.ShadowDecoration.Parent = this.searchBtn;
+            this.searchBtn.Size = new System.Drawing.Size(287, 45);
+            this.searchBtn.TabIndex = 4;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.Click += new System.EventHandler(this.seachBtn_Click);
             // 
             // numOfPassgersTB
             // 
@@ -103,7 +108,7 @@
             this.numOfPassgersTB.FocusedState.Parent = this.numOfPassgersTB;
             this.numOfPassgersTB.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.numOfPassgersTB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
-            this.numOfPassgersTB.Location = new System.Drawing.Point(22, 117);
+            this.numOfPassgersTB.Location = new System.Drawing.Point(92, 117);
             this.numOfPassgersTB.Name = "numOfPassgersTB";
             this.numOfPassgersTB.ShadowDecoration.Parent = this.numOfPassgersTB;
             this.numOfPassgersTB.Size = new System.Drawing.Size(159, 36);
@@ -113,7 +118,7 @@
             // gunaPanel1
             // 
             this.gunaPanel1.Controls.Add(this.companyNameLabel);
-            this.gunaPanel1.Location = new System.Drawing.Point(0, 11);
+            this.gunaPanel1.Location = new System.Drawing.Point(70, 11);
             this.gunaPanel1.Name = "gunaPanel1";
             this.gunaPanel1.Size = new System.Drawing.Size(334, 52);
             this.gunaPanel1.TabIndex = 6;
@@ -130,22 +135,80 @@
             this.companyNameLabel.Text = "gunaLabel1";
             this.companyNameLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // searchResultPanel
+            // 
+            this.searchResultPanel.BackColor = System.Drawing.Color.Transparent;
+            this.searchResultPanel.Controls.Add(this.getTicketLbl);
+            this.searchResultPanel.Controls.Add(this.messageLabel);
+            this.searchResultPanel.Controls.Add(this.datePickerValueLbl);
+            this.searchResultPanel.GradientColor1 = System.Drawing.Color.White;
+            this.searchResultPanel.GradientColor2 = System.Drawing.Color.White;
+            this.searchResultPanel.Location = new System.Drawing.Point(0, 0);
+            this.searchResultPanel.Name = "searchResultPanel";
+            this.searchResultPanel.Size = new System.Drawing.Size(481, 218);
+            this.searchResultPanel.TabIndex = 7;
+            this.searchResultPanel.Visible = false;
+            // 
+            // getTicketLbl
+            // 
+            this.getTicketLbl.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(116)))), ((int)(((byte)(212)))));
+            this.getTicketLbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.getTicketLbl.AutoSize = true;
+            this.getTicketLbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.getTicketLbl.LinkColor = System.Drawing.Color.Black;
+            this.getTicketLbl.Location = new System.Drawing.Point(56, 134);
+            this.getTicketLbl.Name = "getTicketLbl";
+            this.getTicketLbl.Size = new System.Drawing.Size(376, 28);
+            this.getTicketLbl.TabIndex = 3;
+            this.getTicketLbl.TabStop = true;
+            this.getTicketLbl.Text = "Try changing the dates of your search.";
+            this.getTicketLbl.Visible = false;
+            this.getTicketLbl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.getTicketLbl_LinkClicked);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.messageLabel.ForeColor = System.Drawing.Color.Black;
+            this.messageLabel.Location = new System.Drawing.Point(130, 77);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(121, 28);
+            this.messageLabel.TabIndex = 2;
+            this.messageLabel.Text = "gunaLabel1";
+            this.messageLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // datePickerValueLbl
+            // 
+            this.datePickerValueLbl.AutoSize = true;
+            this.datePickerValueLbl.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold);
+            this.datePickerValueLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(116)))), ((int)(((byte)(212)))));
+            this.datePickerValueLbl.Location = new System.Drawing.Point(172, 11);
+            this.datePickerValueLbl.Name = "datePickerValueLbl";
+            this.datePickerValueLbl.Size = new System.Drawing.Size(121, 28);
+            this.datePickerValueLbl.TabIndex = 1;
+            this.datePickerValueLbl.Text = "gunaLabel1";
+            this.datePickerValueLbl.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
             // mainUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.searchResultPanel);
             this.Controls.Add(this.gunaPanel1);
             this.Controls.Add(this.numOfPassgersTB);
-            this.Controls.Add(this.seachBtn);
+            this.Controls.Add(this.searchBtn);
             this.Controls.Add(this.datePicker);
             this.Controls.Add(this.travelCB);
             this.Name = "mainUC";
-            this.Size = new System.Drawing.Size(334, 218);
+            this.Size = new System.Drawing.Size(481, 218);
             this.Load += new System.EventHandler(this.mainUC_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numOfPassgersTB)).EndInit();
             this.gunaPanel1.ResumeLayout(false);
             this.gunaPanel1.PerformLayout();
+            this.searchResultPanel.ResumeLayout(false);
+            this.searchResultPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -153,9 +216,13 @@
         #endregion
         private Guna.UI2.WinForms.Guna2ComboBox travelCB;
         private Guna.UI2.WinForms.Guna2DateTimePicker datePicker;
-        private Guna.UI2.WinForms.Guna2Button seachBtn;
+        private Guna.UI2.WinForms.Guna2Button searchBtn;
         private Guna.UI2.WinForms.Guna2NumericUpDown numOfPassgersTB;
         private Guna.UI.WinForms.GunaPanel gunaPanel1;
         private Guna.UI.WinForms.GunaLabel companyNameLabel;
+        private Guna.UI.WinForms.GunaGradient2Panel searchResultPanel;
+        private Guna.UI.WinForms.GunaLabel messageLabel;
+        private Guna.UI.WinForms.GunaLabel datePickerValueLbl;
+        private Guna.UI.WinForms.GunaLinkLabel getTicketLbl;
     }
 }
--- lookForTravelUC/mainUC.cs
+++ lookForTravelUC/mainUC.cs
@@ -19,39 +19,70 @@ namespace lookForTravelUC
         public String companyId { set { _companyId = value; } }
         public String companyName { set { _companyName = value; } }
         public static String connectionString = "Data source = .; initial catalog = transportMangment; integrated security = true";
-        SqlConnection connection = new SqlConnection(connectionString);
+        DataTable table = new DataTable();
         public mainUC()
         {
             InitializeComponent();
         }
         private void mainUC_Load(object sender, EventArgs e) {
+            _companyId = "COMP1000";
+            setDataSources();
+            numOfPassgersTB.Minimum = 1;
             datePicker.MinDate = DateTime.Now;
         }
         private void seachBtn_Click(object sender, EventArgs e) {
-             
-        }
-        public void setDataSources() {
             try {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand()) {
-                    command.Connection = connection;
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.CommandText = "usp_getOriginToDestination";
-                    command.Parameters.AddWithValue("@companyId", _companyId);
-                    using(SqlDataReader reader = command.ExecuteReader()) {
-                        DataTable table = new DataTable();
-                        table.Load(reader);
-                        travelCB.DataSource = table;
-                        travelCB.DisplayMember = "travel";
+                if(table.Rows.Count > 0) {
+                    datePickerValueLbl.Text = datePicker.Value.ToShortDateString();
+                    if (table.Rows[travelCB.SelectedIndex].Field<String>(4).Equals("1")) {
+                        messageLabel.Text = $"{table.Rows[travelCB.SelectedIndex].Field<TimeSpan>(2)} -- {table.Rows[travelCB.SelectedIndex].Field<TimeSpan>(3)}";
+                        getTicketLbl.Text = " Sign In To get a Ticket";
+                        getTicketLbl.Visible = true;
+                        searchResultPanel.Visible = true;
                     }
-                    connection.Dispose();
-                    companyNameLabel.Text = _companyName;
+                    else {
+                        // Sign In To get a Ticket
+                        //No travels left for today
+                        messageLabel.Text = "No travels left for today";
+                        getTicketLbl.Text = "Try changing the dates of your search.";
+                        searchResultPanel.Visible = true;
+                        getTicketLbl.Visible = true;
+                    }
                 }
             }
             catch(Exception ex) {
                 Debug.WriteLine(ex.Message);
             }
         }
-        
+        public void setDataSources() {
+            try {
+                using (SqlConnection connection = new SqlConnection(connectionString)) {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand()) {
+                        command.Connection = connection;
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "usp_getOriginToDestination";
+                        command.Parameters.AddWithValue("@companyId", _companyId);
+                        using (SqlDataReader reader = command.ExecuteReader()) {
+                            table.Load(reader);
+                            travelCB.DataSource = table;
+                            travelCB.DisplayMember = "travel";
+                        }
+                        companyNameLabel.Text = _companyName;
+                    }
+                }           
+            }
+            catch(Exception ex) {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void getTicketLbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            if (getTicketLbl.Text.Equals(" Sign In To get a Ticket"))
+                searchResultPanel.Visible = false;
+            else {
+                // todo logic to run when the user wanna signin
+            }
+        }
     }
 }
